00f555e448ebb21230db956bbedd4ae3194e9982
bandreetto <bandreetto@hotmail.com>
2017-08-10 16:44:33 -0300

    correcting same issue with tests

--- src/NuCredit/tests/service_tests.clj
+++
@@ -1,9 +0,0 @@
-(ns nucredit.tests.service-tests
-  (:require [clojure.test :refer :all]
-            [nucredit.services :refer :all]))
-
-(deftest new-account-test
-  (let [account (create-account "test")]
-    (is account)
-    (is (> (:account-number account) 0))
-    (is (= (:name account) "test"))))
